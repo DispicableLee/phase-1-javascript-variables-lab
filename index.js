d18f5ead55ee2c613dad3485a4bc892e5b573b1a
DispicableLee <lee.robert053@gmail.com>
2022-10-06 13:14:38 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function companyName(anything){
+    let aNything = anything;
+    aNything = "Scuber";
+    return aNything;
+}
+console.log(companyName("anything"))
+
+
+
+function mostProfitableNeighborhood(anything){
+    let anyThing = anything;
+    anyThing = "Chelsea";
+    return anyThing;
+};
+console.log(mostProfitableNeighborhood("blah"))
+
+function companyCeo(anything){
+    let anYthing = anything;
+    anYthing = "Susan Smith";
+    return anYthing
+}
+
+console.log(companyCeo("anything"))
+
+
+function scuberGreetingorFeet(feet){
+    let x = feet;
+    if(x <= 400){
+        return "this one is on me"
+    }else{
+        return "nah dude this one is all you"
+    }
+}
+
+console.log(scuberGreetingorFeet(50))
+
